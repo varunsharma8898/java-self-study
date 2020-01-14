d958b8dcf5b40d3530b367de115092f8b06f3c6e
Varun Sharma <varunsharma8898@gmail.com>
2020-01-14 11:19:20 +0530

    Pingpong using blocking queues

---
+++ src/main/java/com/varun/selfstudy/multithreading/pingpong/PingPongWtihQueues.java
@@ -0,0 +1,54 @@
+package com.varun.selfstudy.multithreading.pingpong;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class PingPongWtihQueues {
+
+    BlockingQueue<String> serving;
+
+    BlockingQueue<String> receiving;
+
+    PingPongWtihQueues(BlockingQueue<String> q1, BlockingQueue<String> q2) {
+        this.serving = q1;
+        this.receiving = q2;
+    }
+
+    private void ping() {
+        while (true) {
+            try {
+                Thread.sleep(1000);
+                String msg = receiving.take();
+                System.out.println(Thread.currentThread().getName() + " " + msg);
+                String newMsg = msg.equals("PING") ? "PONG" : "PING";
+                serving.offer(newMsg);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        final BlockingQueue<String> q1 = new ArrayBlockingQueue<String>(1);
+        final BlockingQueue<String> q2 = new ArrayBlockingQueue<String>(1);
+        q2.offer("PING");
+        Thread t1 = new Thread() {
+            public void run() {
+                PingPongWtihQueues p1 = new PingPongWtihQueues(q1, q2);
+                p1.ping();
+            }
+        };
+
+        Thread t2 = new Thread() {
+            public void run() {
+                PingPongWtihQueues p2 = new PingPongWtihQueues(q2, q1);
+                p2.ping();
+            }
+        };
+
+        t1.start();
+        t2.start();
+    }
+
+}
