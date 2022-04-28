36cf2cc0ca0c76941246fbec6f474aa99e24091e
Sharma, Varun <varunsharma8898@gmail.com>
2022-04-28 11:40:13 +0530

    low level design - parking lot - incomplete

---
+++ src/main/java/com/varun/selfstudy/lowleveldesign/ParkingLot.java
@@ -0,0 +1,30 @@
+package com.varun.selfstudy.lowleveldesign;
+
+public class ParkingLot {
+
+    public static void main(String[] args) {
+        ParkingLot parkingLot = new ParkingLot();
+
+        // add a lot - has address
+        // add a floor
+        // add spots - S/M/L types
+        // getParkingSpotForSize()
+        // parkVehicle() - return ticket
+        // unparkVehicle(ticket) - boolean
+        // isParkingFull ?
+        //
+    }
+
+
+    ParkingLot() {
+        //init
+    }
+
+
+
+
+
+
+
+
+}
