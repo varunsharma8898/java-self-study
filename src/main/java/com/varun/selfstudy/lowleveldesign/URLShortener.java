8585be3331c1ae275ba6276e7167a5dd733ac72c
Sharma, Varun <varunsharma8898@gmail.com>
2022-05-04 11:40:13 +0530

    low level design - url shortner

---
+++ src/main/java/com/varun/selfstudy/lowleveldesign/URLShortener.java
@@ -0,0 +1,182 @@
+package com.varun.selfstudy.lowleveldesign;
+
+import java.util.HashMap;
+import java.util.Random;
+
+public class URLShortener {
+
+    /**
+     *
+     * public class UrlShortener {
+     *     private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+     *     private static final int    BASE     = ALPHABET.length();
+     *
+     *     public static String encode(int num) {
+     *         StringBuilder sb = new StringBuilder();
+     *         while ( num > 0 ) {
+     *             sb.append( ALPHABET.charAt( num % BASE ) );
+     *             num /= BASE;
+     *         }
+     *         return sb.reverse().toString();
+     *     }
+     *
+     *     public static int decode(String str) {
+     *         int num = 0;
+     *         for ( int i = 0; i < str.length(); i++ )
+     *             num = num * BASE + ALPHABET.indexOf(str.charAt(i));
+     *         return num;
+     *     }
+     * }
+     *
+     * */
+
+
+    /*
+     * URL Shortener
+     */
+    // storage for generated keys
+    private HashMap<String, String> keyMap; // key-url map
+
+    private HashMap<String, String> valueMap;// url-key map to quickly check
+
+    // whether an url is
+    // already entered in our system
+    private String domain; // Use this attribute to generate urls for a custom
+
+    // domain name defaults to http://fkt.in
+    private char myChars[]; // This array is used for character to number
+
+    // mapping
+    private Random myRand; // Random object used to generate random integers
+
+    private int keyLength; // the key length in URL defaults to 8
+
+    // Default Constructor
+    URLShortener() {
+        keyMap = new HashMap<>();
+        valueMap = new HashMap<>();
+        myRand = new Random();
+        keyLength = 8;
+        myChars = new char[62];
+        for (int i = 0; i < 62; i++) {
+            int j = 0;
+            if (i < 10) {
+                j = i + 48;
+            } else if (i > 9 && i <= 35) {
+                j = i + 55;
+            } else {
+                j = i + 61;
+            }
+            myChars[i] = (char) j;
+        }
+        domain = "http://fkt.in";
+    }
+
+    // Constructor which enables you to define tiny URL key length and base URL
+    // name
+    URLShortener(int length, String newDomain) {
+        this();
+        this.keyLength = length;
+        if (!newDomain.isEmpty()) {
+            newDomain = sanitizeURL(newDomain);
+            domain = newDomain;
+        }
+    }
+
+    // shortenURL
+    // the public method which can be called to shorten a given URL
+    public String shortenURL(String longURL) {
+        String shortURL = "";
+        if (validateURL(longURL)) {
+            longURL = sanitizeURL(longURL);
+            if (valueMap.containsKey(longURL)) {
+                shortURL = domain + "/" + valueMap.get(longURL);
+            } else {
+                shortURL = domain + "/" + getKey(longURL);
+            }
+        }
+        // add http part
+        return shortURL;
+    }
+
+    // expandURL
+    // public method which returns back the original URL given the shortened url
+    public String expandURL(String shortURL) {
+        String longURL = "";
+        String key = shortURL.substring(domain.length() + 1);
+        longURL = keyMap.get(key);
+        return longURL;
+    }
+
+    // Validate URL
+    // not implemented, but should be implemented to check whether the given URL
+    // is valid or not
+    boolean validateURL(String url) {
+        return true;
+    }
+
+    // sanitizeURL
+    // This method should take care various issues with a valid url
+    // e.g. www.google.com,www.google.com/, http://www.google.com,
+    // http://www.google.com/
+    // all the above URL should point to same shortened URL
+    // There could be several other cases like these.
+    String sanitizeURL(String url) {
+        if (url.substring(0, 7).equals("http://")) {
+            url = url.substring(7);
+        }
+
+        if (url.substring(0, 8).equals("https://")) {
+            url = url.substring(8);
+        }
+
+        if (url.charAt(url.length() - 1) == '/') {
+            url = url.substring(0, url.length() - 1);
+        }
+        return url;
+    }
+
+    /*
+     * Get Key method
+     */
+    private String getKey(String longURL) {
+        String key;
+        key = generateKey();
+        keyMap.put(key, longURL);
+        valueMap.put(longURL, key);
+        return key;
+    }
+
+    // generateKey
+    private String generateKey() {
+        String key = "";
+        boolean flag = true;
+        while (flag) {
+            key = "";
+            for (int i = 0; i <= keyLength; i++) {
+                key += myChars[myRand.nextInt(62)];
+            }
+            // System.out.println("Iteration: "+ counter + "Key: "+ key);
+            if (!keyMap.containsKey(key)) {
+                flag = false;
+            }
+        }
+        return key;
+    }
+
+    // test the code
+    public static void main(String args[]) {
+        URLShortener u = new URLShortener(5, "www.tinyurl.com/");
+        String urls[] = { "www.google.com/", "www.google.com",
+                "http://www.yahoo.com", "www.yahoo.com/", "www.amazon.com",
+                "www.amazon.com/page1.php", "www.amazon.com/page2.php",
+                "www.flipkart.in", "www.rediff.com", "www.techmeme.com",
+                "www.techcrunch.com", "www.lifehacker.com", "www.icicibank.com" };
+
+        for (int i = 0; i < urls.length; i++) {
+            System.out.println("URL:" + urls[i] + "\tTiny: "
+                    + u.shortenURL(urls[i]) + "\tExpanded: "
+                    + u.expandURL(u.shortenURL(urls[i])));
+        }
+    }
+}
